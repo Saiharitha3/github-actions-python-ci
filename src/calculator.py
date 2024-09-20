cb978ac0e30df3ae4cd1c43df6f39de736118ada
Sai Haritha Sardena <sharitha313@gmail.com>
2024-09-20 16:17:59 -0700

    updated python application with github action

--- src/calculator.py
+++ src/calculator.py
@@ -4,5 +4,5 @@ def subtract(a, b):
     return a - b
 def divide (a,b):
     if b == 0:
-        print("divide with 0 is not possible")
+        return "divide with 0 is not possible"
     return a/b
