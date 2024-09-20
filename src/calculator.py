fbb6d764c80721a8863986b8b855d23c8495cd09
Sai Haritha Sardena <sharitha313@gmail.com>
2024-09-19 23:10:22 -0700

    set up python application with github actions

---
+++ src/calculator.py
@@ -0,0 +1,9 @@
+class mathOperations():
+    def add(a, b):
+        return a + b
+    def subtract(a, b):
+        return a - b
+    def divide (a,b):
+        if b == 0:
+            print("divide with 0 is not possible")
+        return a/b
