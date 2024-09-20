dc6c929703041a46f29a6fb5d75f07743d8f47f2
Sai Haritha Sardena <sharitha313@gmail.com>
2024-09-19 23:33:07 -0700

    updated python math application with github action

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,16 +6,18 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
+
     def test_subtract(self):
-        # COMPLETE HERE
-        self.assertEqual(subtract(2, 3), 5)
-        self.assertEqual(subtract(-1, 1), 0)
-        self.assertEqual(subtract(-1, -1), -2)
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(2, 3), -1)  
+        self.assertEqual(subtract(-1, 1), -2)
+
     def test_divide(self):
-        # COMPLETE HERE
-        self.assertEqual(divide(2, 3), 5)
-        self.assertEqual(divide(-1, 1), 1)
-        self.assertEqual(divide(6, 0))
-        
+        self.assertEqual(divide(6, 2), 3)
+        self.assertEqual(divide(5, 2), 2.5)
+        self.assertEqual(divide(2, 3), 2 / 3)  
+        with self.assertRaises(ValueError):
+            divide(5, 0)
+
 if __name__ == '__main__':
-   unittest.main()
+    unittest.main()
